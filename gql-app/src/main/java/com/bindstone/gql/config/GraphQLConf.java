3a0f173fd1a8cfb7fa149ed70fadf96b4b944fba
Mike Schilling <msbindstone@gmail.com>
2019-10-19 21:17:32 +0200

    Init Project

--- gql-app/src/main/java/com/bindstone/gql/config/GraphQLConf.java
+++ gql-app/src/main/java/com/bindstone/gql/config/GraphQLConf.java
@@ -11,3 +11,41 @@ public class GraphQLConf {
         System.out.println("............GraphQLConf...........");
     }
 }
+
+/**
+
+ @Autowired
+ GraphQLDataFetchers graphQLDataFetchers;
+
+ private GraphQL graphQL;
+
+ @PostConstruct
+ public void init() throws IOException {
+ URL url = Resources.getResource("schema.graphqls");
+ String sdl = Resources.toString(url, Charsets.UTF_8);
+ GraphQLSchema graphQLSchema = buildSchema(sdl);
+ this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+ }
+
+ private GraphQLSchema buildSchema(String sdl) {
+ TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+ RuntimeWiring runtimeWiring = buildWiring();
+ SchemaGenerator schemaGenerator = new SchemaGenerator();
+ return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+ }
+
+ private RuntimeWiring buildWiring() {
+ return RuntimeWiring.newRuntimeWiring()
+ .type(newTypeWiring("Query")
+ .dataFetcher("Motorbike", graphQLDataFetchers.getMotorBikesDataFetcher()))
+ .type(newTypeWiring("MotorbikeById")
+ .dataFetcher("id", graphQLDataFetchers.getMotorBikeByIdDataFetcher()))
+ .build();
+ }
+
+ @Bean
+ public GraphQL graphQL() {
+ return graphQL;
+ }
+
+ **/
--- gql-config/src/main/java/com/bindstone/gql/GraphQLConfigRegistrar.java
+++ gql-config/src/main/java/com/bindstone/gql/GraphQLConfigRegistrar.java
@@ -1,14 +1,24 @@
 package com.bindstone.gql;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class GraphQLConfigRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware, ApplicationContextAware {
 
@@ -19,14 +29,10 @@ public class GraphQLConfigRegistrar implements ImportBeanDefinitionRegistrar, Re
     }
 
     @Override
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        System.out.println("........setResourceLoader........");
-    }
+    public void setResourceLoader(ResourceLoader resourceLoader) {}
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
-        System.out.println("........registerBeanDefinitions........");
-
         AnnotationAttributes mapperScanAttrs = AnnotationAttributes
                 .fromMap(importingClassMetadata.getAnnotationAttributes(GraphQLConfig.class.getName()));
         if (mapperScanAttrs != null) {
@@ -40,7 +46,40 @@ public class GraphQLConfigRegistrar implements ImportBeanDefinitionRegistrar, Re
 
     private void scanBasePackages(String[] basePackages) {
         for (String basePackage : basePackages) {
-            System.out.println(basePackage);
+            findAnnotatedClasses(basePackage);
+        }
+    }
+
+    public void findAnnotatedClasses(String scanPackage) {
+        ClassPathScanningCandidateComponentProvider provider = createComponentScanner();
+        for (BeanDefinition beanDef : provider.findCandidateComponents(scanPackage)) {
+            printMetadata(beanDef);
+        }
+    }
+
+    private ClassPathScanningCandidateComponentProvider createComponentScanner() {
+        ClassPathScanningCandidateComponentProvider provider
+                = new ClassPathScanningCandidateComponentProvider(false);
+        provider.addIncludeFilter(new AnnotationTypeFilter(GraphQLService.class));
+        return provider;
+    }
+
+    private void printMetadata(BeanDefinition beanDef) {
+        try {
+            Class<?> cl = Class.forName(beanDef.getBeanClassName());
+
+            GraphQLService findable = cl.getAnnotation(GraphQLService.class);
+            System.out.printf("Found class: %s",
+                    cl.getSimpleName());
+
+            for (Method method : cl.getMethods()) {
+                GraphQLDataFetcher annotation = AnnotationUtils.findAnnotation(method, GraphQLDataFetcher.class);
+                if (Objects.nonNull(annotation)) {
+                    System.out.println(annotation);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Got exception: " + e.getMessage());
         }
     }
 
